e6818175557a47c2e3403b01e3260b92bcb29971
Krishna Meena <106601578+Z3rOGoku@users.noreply.github.com>
2023-10-04 12:40:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function scrollToElement(elementSelector, instance = 0){
+    const elements = document.querySelectorAll(elementSelector);
+
+    if(elements.length > instance){
+        elements[instance].scrollIntoView({ behavior: 'smooth'});
+    }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', ()=> {
+    scrollToElement('.header');
+})
+
+link2.addEventListener('click', ()=> {
+    scrollToElement('.header', 1);
+})
+
+link3.addEventListener('click', ()=> {
+    scrollToElement('.column', 2);
+})
+
+
+
+const Scrollani = document.getElementById("scroll");
+Scrollani.addEventListener('click', ()=> {
+    scrollToElement('.header');
+})
